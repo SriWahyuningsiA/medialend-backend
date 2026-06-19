21fbd464853b6204b3beb36c92ffe988ededefad
SriWahyuningsiA <swhyuni34@gmail>
2026-06-19 10:55:14 +0800

    fix mysql database variable

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
-  process.env.MYSQLDATABASE,
+  process.env.MYSQL_DATABASE,
   process.env.MYSQLUSER,
   process.env.MYSQLPASSWORD,
   {
